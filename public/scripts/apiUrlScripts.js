5d9d25318d8fd8fc0699f7ad15d57feee40c81a0
Malachi Mccormick <malachimccormick@Malachis-MBP.localdomain>
2018-12-03 08:12:16 -0500

    latest

--- public/scripts/apiUrlScripts.js
+++ public/scripts/apiUrlScripts.js
@@ -1,17 +1,49 @@
 
-$("#urlButton").click(function(){
+$("button").click(function(){
+inputUrl = $("input").val();
+const Http = new XMLHttpRequest();
+const googleUrl = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/fbsS&key=' + googleKey
+const longUrl= {longUrl:inputUrl}
 
-    inputUrl = $("#urlInput").val();
-    console.log(inputUrl)
-    googleUrl = 'https://www.googleapis.com/urlshortener/v1/?key=' + googleKey
-    console.log(googleUrl)
-             $.post((googleUrl), function (data) {
-                         console.log(data)
-                         
-                        longUrl= {"longUrl": "http://www.google.com/"}
-                         console.log(longUrl)
-             })
+$.ajax({
+    url: googleUrl,
+    type: "POST",
+    contentType: "application/json",
+    data: longUrl,
+    dataType: 'json',
+    encode: true,
+        success: function (result){
+            
+        console.log(result)
+    }
 })
+
+
+
+// Http.open("get", googleUrl,);
+// Http.send(inputUrl);
+// Http.onreadystatechange = (e) => {
+//     console.log(e)
+//     console.log(Http.responseText)
+// }
+
+    // inputUrl = $("input").val();
+    // console.log(inputUrl)
+    // googleUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=' + googleKey
+    // console.log(googleUrl)
+    //          $.post((googleUrl), {shortUrl: inputUrl}
+             
+    //         //  function(data) {
+                 
+    //         //      Content-Type; application/json
+    //         //      {longUrl; inputUrl}
+    //         //              document.write(data)
+                         
+                        
+                         
+    //          )
+})
+ 
 
 
 
